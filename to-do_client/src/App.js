2bcd999a083e6e04d2ecfebe213900e875c7bfcf
Garrett Rasko-Martinis <garrettrm9@gmail.com>
2018-03-09 04:09:58 -0500

    starting React ... set up GET call, response.data succesful

--- to-do_client/src/App.js
+++ to-do_client/src/App.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  
+  constructor(props){
+    super(props)
+    this.state = {to_dos: {to_do: ""}}
+  }
+
+  getToDos(){
+    axios({url: 'http://localhost:3000/to_dos'})
+    .then(response => {
+      console.log('GET all: ', response.data)
+      // this.setState
+    })
+  }
+
+  componentDidMount(){
+    this.getToDos()
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Get your shit done!</h1>
       </div>
     );
   }
