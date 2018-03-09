2cea2eddfefcd76b3caafd1479612b5194bc0775
Garrett Rasko-Martinis <garrettrm9@gmail.com>
2018-03-09 03:51:23 -0500

    finished POST, tested

--- to-do_server/app/controllers/to_dos_controller.rb
+++ to-do_server/app/controllers/to_dos_controller.rb
@@ -1,6 +1,18 @@
 class ToDosController < ApplicationController
-    def index
+  def index
     toDos = ToDo.all
     render json: toDos
   end
+
+  def create
+    toDo = ToDo.create!(toDo_params)
+  end
+
+  private
+
+  def toDo_params
+    params.require(:to_dos).permit(:to_do)
+  end
+
 end
+
